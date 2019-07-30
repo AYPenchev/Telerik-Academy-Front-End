226cc8c771cc96ff6f89ce71ce15a6bc44126ef0
Asen Penchev <asen.penchev@grafixoft.com>
2019-07-30 05:25:30 -0400

    JavaScript Basics - Homework Data types and variable - Task 1

---
+++ JS/DataTypesAndVariables/JavaScriptLiterals.js
@@ -0,0 +1,73 @@
+// Task 1
+let int = 5,
+    double = 4.219381231,
+    float = 4.3,
+    bool = true,
+    object = {},
+    undefinedVar = undefined,
+    string1 = "string1",
+    string2 = 'string2',
+    string3 = `string3`,
+    array = [],
+    nullVar = null;
+
+// Task 2
+let quotedText = `'How you doin'?', Joey said'.`;
+console.log(quotedText);
+
+// Task 3,4
+console.log(typeof(int));
+console.log(typeof(double));
+console.log(typeof(float));
+console.log(typeof(bool));
+console.log(typeof(object));
+console.log(typeof(undefinedVar));
+console.log(typeof(string1));
+console.log(typeof(string2));
+console.log(typeof(string3));
+console.log(typeof(array));
+console.log(typeof(nullVar));
+
+// Task 5
+let str1 = '1234',
+    str2 = '1238abc',
+    str3 = '0.15',
+    str4 = '3.14ivan',
+    str5 = 'Infinity',
+    str6 = '99999999999999999999';
+
+console.log(parseInt(str1));
+console.log(parseFloat(str1));
+console.log(Number(str1));
+console.log(+str1);
+console.log(str1 | 0);
+
+console.log(parseInt(str2));
+console.log(parseFloat(str2));
+console.log(Number(str2));
+console.log(+str2);
+console.log(str2 | 0);
+
+console.log(parseInt(str3));
+console.log(parseFloat(str3));
+console.log(Number(str3));
+console.log(+str3);
+console.log(str3 | 0);
+
+console.log(parseInt(str4));
+console.log(parseFloat(str4));
+console.log(Number(str4));
+console.log(+str4);
+console.log(str4 | 0);
+
+console.log(parseInt(str5));
+console.log(parseFloat(str5));
+console.log(Number(str5));
+console.log(+str5);
+console.log(str5 | 0);
+
+console.log(parseInt(str6));
+console.log(parseFloat(str6));
+console.log(Number(str6));
+console.log(+str6);
+console.log(str6 | 0);
