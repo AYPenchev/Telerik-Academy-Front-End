ee0bf3498818d6fc7ab5bb127f7fdafdbe1e2aff
Asen Penchev <asen.penchev@grafixoft.com>
2019-07-30 09:04:58 -0400

    JavaScript Basics - Homework Operators - Task 3

---
+++ JS101/Operators/Rectangles.js
@@ -0,0 +1,6 @@
+function RectangleAreaAndPerimeter(sidesOfRectangle) {
+    console.log(`Area: ${sidesOfRectangle[0] * sidesOfRectangle[1]}, Perimeter: ${2*(sidesOfRectangle[0] + sidesOfRectangle[1])}`);
+}
+
+let sides = [5, 7]
+RectangleAreaAndPerimeter(sides);
