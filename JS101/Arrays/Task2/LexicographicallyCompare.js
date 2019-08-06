52d8840fd01f981a2d89722abd8822ea3e866099
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-06 09:56:58 -0400

    JavaScript Basics - Homework Arrays - Task 2

---
+++ JS101/Arrays/Task2/LexicographicallyCompare.js
@@ -0,0 +1,26 @@
+function LexCompare() {
+    let firstCharArray = [],
+        secondCharArray = [],
+        isFirstArrayLexBigger = false;
+
+    firstCharArray = window.prompt('Enter first char array:', 'Char array:');
+    secondCharArray = window.prompt('Enter second char array:', 'Char array:');
+
+    for (let i = 0; i < firstCharArray.length && i < secondCharArray.length; i++) {
+
+        if (firstCharArray[i].localeCompare(secondCharArray[i]) === 1) {
+            return '>';
+        } else if (firstCharArray[i].localeCompare(secondCharArray[i]) === -1) {
+            return '<';
+        }
+    }
+    if (firstCharArray.length === secondCharArray.length) {
+        return '=';
+    } else if (firstCharArray.length < secondCharArray.length) {
+        return '>';
+    } else {
+        return '<';
+    }
+}
+
+console.log(LexCompare());
