0b524e32cbbfdf2bc984e3caad1da54d34d06d34
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-07 14:51:16 -0400

    JavaScript Basics - Homework Functions- Task 5

---
+++ JS101/Functions/Task05/LargerThanNeighbours.js
@@ -0,0 +1,33 @@
+function largerThanNeighbours() {
+    let countLargerThanNeighbours = 0,
+        largerThanNeighbourArray = [],
+        newNumber,
+        largerThanNeighbourArrayLen = parseInt(window.prompt("Enter array size: "));
+
+    for (let i = 0; i < largerThanNeighbourArrayLen; i++) {
+        newNumber = parseInt(window.prompt('Enter element', 'One number representing an element'));
+        largerThanNeighbourArray.push(newNumber);
+    }
+
+    for (let i = 0; i < largerThanNeighbourArrayLen; i++) {
+        if (i != 0 && i != largerThanNeighbourArrayLen - 1) {
+            if (largerThanNeighbourArray[i] > largerThanNeighbourArray[i - 1] && largerThanNeighbourArray[i] > largerThanNeighbourArray[i + 1]) {
+                countLargerThanNeighbours++;
+            }
+        }
+        // Check if first and last numbers are larger than their only neighbour
+        else if (i == 0) {
+            if (largerThanNeighbourArray[i] > largerThanNeighbourArray[i + 1]) {
+                countLargerThanNeighbours++;
+            }
+        } else if (i == largerThanNeighbourArrayLen - 1) {
+            if (largerThanNeighbourArray[i] > largerThanNeighbourArray[i - 1]) {
+                countLargerThanNeighbours++;
+            }
+        }
+
+    }
+    return countLargerThanNeighbours;
+}
+
+console.log(largerThanNeighbours());
