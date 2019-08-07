0236a947849d6496da62424bbb96527a331105cf
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-07 14:35:09 -0400

    JavaScript Basics - Homework Functions- Task 4

---
+++ JS101/Functions/Task04/AppearanceCount.js
@@ -0,0 +1,22 @@
+function appearanceCount() {
+    let appearanceArray = [],
+        newNumber,
+        counterOfSearchedNum = 0,
+        appearanceArrayLen = parseInt(window.prompt("Enter array size: ")),
+        searchedNum = parseInt(window.prompt('Enter searched number: '));
+
+    for (let i = 0; i < appearanceArrayLen; i++) {
+        newNumber = parseInt(window.prompt('Enter element', 'One number representing an element'));
+        appearanceArray.push(newNumber);
+    }
+
+    for (const number of appearanceArray) {
+        if (number === searchedNum) {
+            counterOfSearchedNum++;
+        }
+    }
+
+    console.log(counterOfSearchedNum);
+}
+
+appearanceCount();
