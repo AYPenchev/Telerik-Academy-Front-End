d7c449544645a7068bb4b8e58f2e54a2dca9941b
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-07 09:01:47 -0400

    JavaScript Basics - Homework Arrays - Task 8

---
+++ JS101/Arrays/Task8/SubsetSum.js
@@ -0,0 +1,43 @@
+'use strict'
+
+function binarySearchMain() {
+    let arraySum = 0,
+        startInx = null,
+        endInx = null,
+        givenSum = parseInt(window.prompt("Enter Sum: ")),
+        findSumArrayLen = parseInt(window.prompt("Enter array size: ")),
+        findSumArray = [],
+        newNumber,
+        sumElements = [];
+
+    for (let i = 0; i < findSumArrayLen; i++) {
+        newNumber = parseInt(window.prompt('Enter element', 'One number representing an element'));
+        findSumArray.push(newNumber);
+    }
+
+    for (let i = 0; i < findSumArrayLen; i++) {
+        arraySum = 0;
+
+        for (let j = i; j < findSumArrayLen; j++) {
+            arraySum += findSumArray[j];
+            if (arraySum === givenSum) {
+                startInx = i;
+                endInx = j + 1;
+                i = findSumArrayLen;
+                break;
+            }
+        }
+    }
+
+    if (startInx !== null && endInx !== null) {
+        for (let i = startInx; i < endInx; i++) {
+            sumElements.push(findSumArray[i]);
+        }
+
+        console.log(`Elements - ${sumElements.join('+')} Sum = ${givenSum} YES`);
+    } else {
+        console.log("Sum not found!");
+    }
+}
+
+binarySearchMain();
