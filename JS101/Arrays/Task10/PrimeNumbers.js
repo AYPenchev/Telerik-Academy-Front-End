3c87915a7a909c74d389925a8e2b1726c9773060
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-07 09:52:55 -0400

    JavaScript Basics - Homework Arrays - Task 9

---
+++ JS101/Arrays/Task10/PrimeNumbers.js
@@ -0,0 +1,28 @@
+'use strict'
+
+function PrimeNumber() {
+    let n = parseInt(window.prompt('Enter n: ')),
+        prime = [];
+
+    for (let i = 0; i < n + 1; i++) {
+        prime[i] = true;
+    }
+
+    for (let p = 2; p * p < n; p++) {
+        if (prime[p] == true) {
+            for (let i = p * p; i <= n; i += p) {
+                prime[i] = false;
+            }
+        }
+    }
+
+    for (let i = n; i >= 2; i--) {
+        if (prime[i] == true) {
+            console.log(i);
+            break;
+        }
+    }
+
+}
+
+PrimeNumber();
