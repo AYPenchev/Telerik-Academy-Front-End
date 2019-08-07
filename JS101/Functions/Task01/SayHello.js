94b5b45e66c510259ef557c3a161252e2bc10186
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-07 12:41:12 -0400

    JavaScript Basics - Homework Functions- Task 1

--- JS101/Functions/Task01/SayHello.js
+++ JS101/Functions/Task01/SayHello.js
@@ -1,6 +1,8 @@
-function SayHello() {
+'use strict'
+
+function sayHello() {
     let userName = window.prompt('Enter your name:', 'Your name');
     console.log(`Hello, ${userName}!`);
 }
 
-SayHello();
+sayHello();
