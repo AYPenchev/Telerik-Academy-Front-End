a2ad47e34ffb315257c040fd107b69fbd2603824
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-13 08:46:09 -0400

    JavaScript OOP - Homework Functions-Expressions-IIFEs - Task 01

---
+++ JS_OOP/Functions_Expressions_IIFEs/Task01/main.js
@@ -0,0 +1,30 @@
+'use strict'
+
+function sum() {
+    let sum = 0;
+
+    if (arguments[0] === undefined) {
+        throw 'Parameter is not passed (undefined)!';
+    }
+
+    if (arguments[0] && arguments[0].constructor === Array && arguments[0].length === 0) {
+        return null;
+    }
+
+    for (let number of arguments[0]) {
+        number = parseInt(number);
+
+        if (isNaN(number)) {
+            throw 'Element is not convertible to Number!';
+        }
+
+        sum += number;
+    }
+    return sum;
+}
+
+try {
+    console.log(sum([1, '2', "3p", 4]));
+} catch (e) {
+    console.log(e);
+}
