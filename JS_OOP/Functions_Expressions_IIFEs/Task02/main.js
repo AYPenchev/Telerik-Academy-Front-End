8ee4404358d4a7c304aa9bcbc68ae49a20c8a10e
Asen Penchev <asen.penchev@grafixoft.com>
2019-08-13 09:27:08 -0400

    JavaScript OOP - Homework Functions-Expressions-IIFEs - Task 02

---
+++ JS_OOP/Functions_Expressions_IIFEs/Task02/main.js
@@ -0,0 +1,45 @@
+'use strict'
+
+function primeNumbersInRange() {
+    if (arguments[0] === undefined || arguments[1] === undefined) {
+        throw 'The range params is missing!';
+    }
+    let startNumber;
+    let endNumber;
+    startNumber = parseInt(arguments[0]);
+    endNumber = parseInt(arguments[1]);
+
+    if (isNaN(startNumber) || isNaN(endNumber)) {
+        throw 'The range params is not convertible to Number!';
+    }
+
+    if (startNumber === 1) {
+        startNumber = 2;
+    }
+
+    let prime = [];
+
+    for (let i = startNumber; i < endNumber + 1; i++) {
+        prime[i] = true;
+    }
+
+    for (let p = startNumber; p * p < endNumber; p++) {
+        if (prime[p] == true) {
+            for (let i = p * p; i <= endNumber; i += p) {
+                prime[i] = false;
+            }
+        }
+    }
+
+    for (let i = startNumber; i <= endNumber; i++) {
+        if (prime[i] == true) {
+            console.log(i);
+        }
+    }
+}
+
+try {
+    primeNumbersInRange(1, 100);
+} catch (e) {
+    console.log(e);
+}
